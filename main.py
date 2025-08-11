33f3e05ce143863b826be5680fc020e9a4ed6b6a
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-08-11 15:31:23 +0530

    view created

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()  # FastAPI object
+
+def load():
+    with open("patients.json","r") as f:
+        data = json.load(f)
+    return data
 
 # creating route # Endpoint
 @app.get("/")
 def hello():
-    return {"message":"Hello, everyone"}
+    return {"message":"Patient Management System API ."}
 
 @app.get("/about")
 def about():
-    return {"message":"Hello, this is a campusx website ."}
+    return {"message":"A fully functional API, to manage your patient's record."}
 
+@app.get("/view")
+def view():
+    data = load()
+    return data
